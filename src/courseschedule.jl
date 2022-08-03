071f13d59efb5157d6094948b4945ae9c7bf2a4a
Neel Smith <dnsmith.neel@gmail.com>
2022-08-03 13:08:06 -0400

    Compute class meeting days

--- src/courseschedule.jl
+++ src/courseschedule.jl
@@ -1,22 +1,58 @@
+@enum MeetingPattern MWF TTh
+const patternDict = Dict(
+    "MWF" => MWF,
+    "TTH" => TTh
+)
+
 
 struct CourseSchedule
     title
     weeks::Int64
     firstday::Date
-    schedule
+    lastday::Date
+    meetson::MeetingPattern
     fixeddates
 end
 
+function classes(schedule::CourseSchedule)
+    dr = schedule.firstday:Day(1):schedule.lastday
+    if schedule.meetson == MWF
+        mwfdays = filter(dr) do d
+            Dates.dayofweek(d) == Dates.Mon ||
+            Dates.dayofweek(d) == Dates.Wed ||
+            Dates.dayofweek(d) == Dates.Fri
+        end
+        mwfdays
+        
+    elseif schedule.meetson == TTh
+        tthdays = filter(dr) do d
+            Dates.dayofweek(d) == Dates.Tue ||
+            Dates.dayofweek(d) == Dates.Thu
+        end
+        tthdays
+
+    else
+        @warn("Class schedule not implemented for meeting pattern $(schedule.meetson)")
+        nothing
+    end
+end
+
+
+
+"""Construct a `CourseSchedule` from configuration file.
+"""
 function courseSchedule(configfile)
     config = TOML.parsefile(configfile)
     title = config["courseTitle"]
     weeks = config["totalWeeks"]
     firstday = Date(config["firstDay"])
-    schedule = config["schedule"]
+    lastday = Date(config["lastDay"])
+    schedule = patternDict[uppercase(config["schedule"])]
 
 
-    CourseSchedule(title, weeks, firstday, schedule,
+    CourseSchedule(title, weeks, firstday, lastday, schedule,
         config["fixedDates"]
     )
     
 end
+
